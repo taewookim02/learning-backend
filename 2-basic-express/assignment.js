7305549685cc11f954b645153c21634f314ddff2
taewookim02 <taewookim02@gmail.com>
2022-08-19 02:50:58 +0800

    feat: express assignment done

---
+++ 2-basic-express/assignment.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/users', (req, res, next) => {
+  res.send("<p>This is for 'users' </p>");
+});
+
+app.use('/', (req, res, next) => {
+  res.send("<p>This is for '/' </p>");
+});
+
+app.use((req, res, next) => {
+  console.log('Hi I am middleware 1');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Hi I am middleware 2');
+  res.send('<p>Dummy response</p>');
+});
+
+app.listen(3000);
