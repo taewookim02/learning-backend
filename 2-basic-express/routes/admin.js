6f9385644c865d4d340671cc67d7efb1d8feb49a
taewookim02 <taewookim02@gmail.com>
2022-08-19 12:47:58 +0800

    feat: common directory handling with app.use

--- 2-basic-express/routes/admin.js
+++ 2-basic-express/routes/admin.js
@@ -2,17 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.send(`<html>
     <body>
-      <form action="/product" method="POST">
+      <form action="/admin/add-product" method="POST">
         <input type="text" name="title"><button type="submit">Add product</button>
       </form>
     </body>
   </html>`);
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   console.log(req.body); // { title: 'book' }
   res.redirect('/');
 });
