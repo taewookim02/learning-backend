9e7bf7c3817cfd24e52ff9628145221716c37a0c
taewookim02 <taewookim02@gmail.com>
2022-08-20 01:09:32 +0800

    feat: dynamic html with pug

--- 3-dynamic-content/app.js
+++ 3-dynamic-content/app.js
@@ -3,19 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes); //adminData because multiple exports
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: '404 Error' });
 });
 
 app.listen(3000);
