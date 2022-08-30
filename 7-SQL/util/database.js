14886043dfa0f96ca1f729338dc47b2cd6bb157e
taewookim02 <taewookim02@gmail.com>
2022-08-30 22:07:21 +0800

    feat: setting up sequelize for sql

---
+++ 7-SQL/util/database.js
@@ -0,0 +1,21 @@
+/* using mysql2
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'node_complete',
+  password: 'nodecomplete',
+});
+
+module.exports = pool.promise();
+*/
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node_complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost',
+});
+
+module.exports = sequelize;
