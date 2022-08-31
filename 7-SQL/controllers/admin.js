eedd84d04108df93cb83461e5d10f2badaea9fbf
taewookim02 <taewookim02@gmail.com>
2022-09-01 02:30:56 +0800

    chore: associating tables and creating them

--- 7-SQL/controllers/admin.js
+++ 7-SQL/controllers/admin.js
@@ -13,14 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct();
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-    // userId: req.user.id,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+      // userId: req.user.id,
+    })
     .then(result => {
       console.log('Created Product');
       res.redirect('/admin/products');
@@ -44,8 +44,11 @@ exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) return res.redirect('/');
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0];
       if (!product) return res.redirect('/');
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
@@ -88,7 +91,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  // Product.findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
--- 7-SQL/models/cart.js
+++ 7-SQL/models/cart.js
@@ -1,3 +1,19 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Cart = sequelize.define('cart', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+});
+
+module.exports = Cart;
+
+/* mysql
 const fs = require('fs');
 const path = require('path');
 
@@ -71,3 +87,4 @@ module.exports = class Cart {
     });
   }
 };
+ */
