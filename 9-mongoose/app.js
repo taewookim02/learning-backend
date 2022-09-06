211566c7485a0f955bd32d43b10ef119eb5c9e24
taewookim02 <taewookim02@gmail.com>
2022-09-07 01:53:00 +0800

    chore: add mongoose

---
+++ 9-mongoose/app.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('63161f4a32c7788b07bbc7d0')
+    .then(user => {
+      console.log('DISPLAY USERCART', user.cart);
+      if (!user.cart) {
+        console.log('No cart.');
+        user.cart = { items: [] };
+      }
+      req.user = new User(user.username, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
