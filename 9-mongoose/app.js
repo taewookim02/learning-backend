71353221ddcaf6ac48c5055b81cad72038950000
taewookim02 <taewookim02@gmail.com>
2022-09-07 02:02:00 +0800

    chore: commenting all mongodb drivers to start fresh with mongoose

--- 9-mongoose/app.js
+++ 9-mongoose/app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -37,6 +37,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://user1:TszxF9Leg43g22W0@cluster0.stgbtnt.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
