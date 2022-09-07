5c77c1e5754be3d0109da2f1a71a67e2dd4563c7
taewookim02 <taewookim02@gmail.com>
2022-09-07 13:38:06 +0800

    feat: get cart with populate method

--- 9-mongoose/controllers/shop.js
+++ 9-mongoose/controllers/shop.js
@@ -43,22 +43,13 @@ exports.getIndex = (req, res, next) => {
     .catch(err => {
       console.log(err);
     });
-  // mysql
-  // Product.fetchAll()
-  //   .then(([rows, fieldData]) => {
-  //     res.render('shop/index', {
-  //       prods: rows,
-  //       pageTitle: 'Shop',
-  //       path: '/',
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
