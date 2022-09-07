d7c20d49d90cf6494f543aec8f34e1a209f9cba4
taewookim02 <taewookim02@gmail.com>
2022-09-07 14:14:13 +0800

    feat: mongoose wrap up

--- 9-mongoose/controllers/shop.js
+++ 9-mongoose/controllers/shop.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
+
 // const Cart = require('../models/cart');
-// const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -77,7 +78,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
@@ -86,16 +87,32 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .addOrder()
+    .populate('cart.items.productId')
+    .then(user => {
+      console.log();
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
     .then(result => {
+      req.user.clearCart();
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ 'user.userId': req.user._id })
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
