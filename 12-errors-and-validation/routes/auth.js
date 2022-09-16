3f4b64dccd6de902daa8d310e276c3ddbf8615f4
taewookim02 <taewookim02@gmail.com>
2022-09-17 00:26:37 +0800

    feat: add email, password length and identicality validation in signup

--- 12-errors-and-validation/routes/auth.js
+++ 12-errors-and-validation/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -15,7 +15,29 @@ router.post('/login', authController.postLogin);
 
 router.post(
   '/signup',
-  check('email').isEmail().withMessage('Please enter a valid email'),
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        if (value === 'test@test.com') {
+          throw new Error('This email address is forbidden.');
+        }
+        return true;
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords have to match!');
+      }
+      return true;
+    }),
+  ],
   authController.postSignup
 );
 
