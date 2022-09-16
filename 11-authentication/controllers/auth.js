ae238b8c8904902c55ae431cbaede2dbaea70a14
taewookim02 <taewookim02@gmail.com>
2022-09-16 13:07:34 +0800

    fix: 2 resolves being sent to 1 request

--- 11-authentication/controllers/auth.js
+++ 11-authentication/controllers/auth.js
@@ -1,7 +1,10 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 
 const User = require('../models/user');
+const user = require('../models/user');
 
 const transport = nodemailer.createTransport({
   host: 'smtp.mailtrap.io',
@@ -116,5 +119,60 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'reset',
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.');
+          return res.redirect('/reset');
+        }
+
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user
+          .save()
+          .then(() => {
+            res.redirect('/');
+            const msg = {
+              to: req.body.email,
+              from: 'shop@nodecomplete.com',
+              subject: 'Password reset',
+              html: `  
+                <p>You requested a password reset</p>
+                <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+              `,
+            };
+            transport.sendMail(msg);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   });
 };
--- 11-authentication/routes/auth.js
+++ 11-authentication/routes/auth.js
@@ -8,10 +8,14 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
+router.get('/reset', authController.getReset);
+
 router.post('/login', authController.postLogin);
 
 router.post('/signup', authController.postSignup);
 
 router.post('/logout', authController.postLogout);
+
+router.post('/reset', authController.postReset);
 
 module.exports = router;
