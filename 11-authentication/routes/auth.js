8973adafd4c4bec1b52dd55a64257f23b5dbc21b
taewookim02 <taewookim02@gmail.com>
2022-09-16 17:38:53 +0800

    feat: reset password with email link

--- 11-authentication/routes/auth.js
+++ 11-authentication/routes/auth.js
@@ -18,4 +18,8 @@ router.post('/logout', authController.postLogout);
 
 router.post('/reset', authController.postReset);
 
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
+
 module.exports = router;
