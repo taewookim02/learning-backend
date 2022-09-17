38ecd9ff4e2e15ba35133e044395ba7067dbae9e
taewookim02 <taewookim02@gmail.com>
2022-09-17 12:12:44 +0800

    feat: keeping user input in signup after submit

--- 12-errors-and-validation/controllers/auth.js
+++ 12-errors-and-validation/controllers/auth.js
@@ -41,6 +41,11 @@ exports.getSignup = (req, res, next) => {
     path: '/signup',
     pageTitle: 'Signup',
     errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
   });
 };
 
@@ -93,6 +98,11 @@ exports.postSignup = (req, res, next) => {
       path: '/signup',
       pageTitle: 'Signup',
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      },
     });
   }
 
