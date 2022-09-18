d314e53447a0ac1b0da612b04d560f891424a290
Tae Woo Kim <taewookim02@gmail.com>
2022-09-19 03:05:21 +0800

    chore: add setHeader() to app.js for CORS

--- 14-rest-api/app.js
+++ 14-rest-api/app.js
@@ -8,6 +8,16 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form></form>
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
