e34fa8fb9c5c9cb1d8351d4ca9faefc18c56121c
Tae Woo Kim <taewookim02@gmail.com>
2022-09-21 22:13:46 +0800

    feat: post images and fetch images from db

--- 14-rest-api/controllers/feed.js
+++ 14-rest-api/controllers/feed.js
@@ -24,13 +24,19 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  if (!req.file) {
+    const error = new Error('No image provided');
+    error.statusCode = 422;
+    throw error;
+  }
 
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/frog.jpeg',
+    imageUrl: imageUrl,
     creator: { name: 'Taewoo' },
   });
   post
