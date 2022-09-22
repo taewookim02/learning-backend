cb5a54b26eb7ef48780e05c4d3adf04f030f59fe
Tae Woo Kim <taewookim02@gmail.com>
2022-09-23 01:19:14 +0800

    chore: delete images

--- 14-rest-api/controllers/auth.js
+++ 14-rest-api/controllers/auth.js
@@ -16,14 +16,14 @@ exports.signup = async (req, res, next) => {
   const name = req.body.name;
   const password = req.body.password;
   try {
-    const hasedPw = await bcrypt.hash(password, 12);
+    const hashedPw = await bcrypt.hash(password, 12);
 
     const user = new User({
       email: email,
       password: hashedPw,
       name: name,
     });
-    await user.save();
+    const result = await user.save();
 
     res.status(201).json({ message: 'User created', userId: result._id });
   } catch (err) {
